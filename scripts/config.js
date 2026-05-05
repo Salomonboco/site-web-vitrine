f1a1f7ea4ece6282d7d998e40238f0a4cb89550f
BOCO SALOMON <boco0075620@univmetiers.ci>
2026-05-05 01:44:29 +0000

    Premier commit - Site vitrine

---
+++ scripts/config.js
@@ -0,0 +1,10 @@
+/*********************************************************************************
+ * 
+ * Ce fichier contient toutes les constantes nécessaires au fonctionnement du jeu.
+ * En particulier les listes de mots et de phrases proposés à l'utilisateur
+ * 
+ *********************************************************************************/
+
+// Déclaration des tableaux contenant les listes des mots proposés à l'utilisateur
+const listeMots = ["Mystère", "Bibliothèque", "Ordinateur", "Aventure", "Éphémère"]
+const listePhrases = ["Le soleil brille !", "Les oiseaux chantent", "La patience est une vertu", "L'aventure commence", "La persévérance une clé"]
